ab3263500f16ef5ec96b24ecc5b4df216ee33de3
John Evans <johne103@hotmail.com>
2016-11-17 18:01:10 +0000

    API Facebook Login

--- config/routes.js
+++ config/routes.js
@@ -2,10 +2,16 @@ const router = require('express').Router();
 const authController = require('../controllers/auth');
 const twitterController = require('../controllers/twitterController');
 const tflController = require('../controllers/tfl');
+const oauthController =
+require('../controllers/oauth');
+// const secureRoute = require('../lib/secureRoute');
 
 router
   .post('/login', authController.login)
   .post('/register', authController.register)
+  .post('/auth/facebook', oauthController.facebook)
+  .post('/auth/twitter', oauthController.twitter)
+  // .post('/auth/instagram', oauthController.instagram)
   .get('/status', tflController.status);
 
   //TWITTER ROUTES
--- src/js/app.js
+++ src/js/app.js
@@ -1,42 +1,50 @@
 angular
   .module('twitterForLondon', ['ngResource', 'ui.router', 'satellizer'])
-  .config(Router)
-  .config(Auth);
-
+    .config(Router)
+    .config(Auth);
 
 
 Router.$inject = ['$stateProvider', '$urlRouterProvider'];
-function Router($stateProvider, $urlRouterProvider) {
+function Router($stateProvider,   $urlRouterProvider) {
   $stateProvider
-  .state('usersIndex', {
-    url: '/users',
-    templateUrl: '/templates/usersIndex.html',
-    controller: 'UsersIndexController as user'
-  })
-  .state('linesIndex', {
-    url: '/lines',
-    templateUrl: '/templates/linesIndex.html',
-    controller: 'LinesIndexController as linesIndex'
-  })
-  .state('register', {
-    url: '/register',
-    templateUrl: '/templates/register.html',
-    controller: 'RegisterController as register'
-  })
+    .state('register', {
+      url: '/register',
+      templateUrl: '/templates/register.html',
+      controller: 'RegisterController as register'
+    })
   .state('login', {
     url: '/login',
     templateUrl: '/templates/login.html',
     controller: 'LoginController as login'
+  })
+  .state('linesIndex', {
+    url: '/linesindex',
+    templateUrl: '/templates/linesIndex.html',
+    controller: 'LinesIndexController as linesIndex'
   });
 
-  $urlRouterProvider.otherwise('/');
+  $urlRouterProvider.otherwise('/lines');
 }
 
 Auth.$inject = ['$authProvider'];
+
 function Auth($authProvider) {
   $authProvider.loginUrl = '/login';
   $authProvider.signupUrl = '/register';
 
   $authProvider.tokenPrefix = '';
 
+  $authProvider.facebook({
+    clientId: '360279380977935'
+  });
+  //
+  // $authProvider.github({
+  //   clientId: '1ce21c481fa0a5dc7af0'
+  // });
+  //
+  // $authProvider.instagram({
+  //   clientId: 'Instagram Client ID'
+  // });
+
+  //No additional setup required for Twitter
 }
