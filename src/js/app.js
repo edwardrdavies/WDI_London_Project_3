c53e1baeb9cd2d91b2658cb7b7e029965108bba7
William Hilton <williamjchilton@gmail.com>
2016-11-17 11:16:23 +0000

    line statuses dynamically added  on lineIndexe

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,19 @@
 angular
-  .module('twitterForLondon', []);
+  .module('twitterForLondon', ['ngResource', 'ui.router', 'satellizer'])
+  .config(Router);
+
+Router.$inject = ['$stateProvider', '$urlRouterProvider'];
+function Router($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('usersIndex', {
+    url: '/users',
+    templateUrl: '/templates/usersIndex.html',
+    controller: 'UsersIndexController as user'
+  })
+  .state('linesIndex', {
+    url: '/lines',
+    templateUrl: '/templates/linesIndex.html',
+    controller: 'LinesIndexController as linesIndex'
+  });
+  $urlRouterProvider.otherwise('/');
+}
