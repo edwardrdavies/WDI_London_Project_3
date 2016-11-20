5df381d584677cdd153a21cecdd30aabe358a9af
Lee Cash <leealexandercash@gmail.com>
2016-11-20 15:43:39 +0000

    Changed username referenced model for message to User

--- models/message.js
+++ models/message.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
   tflId: [{ type: mongoose.Schema.ObjectId, ref: 'Line' }],
-  username: [{ type: mongoose.Schema.ObjectId, ref: 'Line' }],
+  username: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
   messageText: {type: String, required: true }
 });
 
